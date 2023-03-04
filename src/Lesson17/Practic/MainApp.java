255fa09ae03aa27ba7277f9241af9c52a65abfe7
cahaciyev <cahaciyev@gmail.com>
2023-03-04 22:28:09 +0400

    EncoderAndDecoderApp

--- src/Lesson17/Practic/MainApp.java
+++ src/Lesson17/Practic/MainApp.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MainApp {
+public class Main {
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
         personList.add(new Person(45));
